59847df8f59cd2a59685e1922a0a0731372a0cc2
Thomas Powell <t.powell.meto@gmail.com>
2017-01-17 15:36:00 +0000

    cleanup initial commit

--- src/main/js/handler.js
+++ src/main/js/handler.js
@@ -1,12 +1,7 @@
 'use strict';
 
-import Validator from "./validators/Validator";
-import * as schema from "./schemas/request.json";
-import JsonResponse from "./responses/JsonResponse";
-import TextResponse from "./responses/TextResponse";
-import * as HttpStatus from "http-status-codes";
 
-const validator = new Validator(schema);
+import JsonResponse from "./responses/JsonResponse";
 
 module.exports.apiai = (event, context, callback) => {
 
